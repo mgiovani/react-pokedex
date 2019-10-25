360dca61036367339377dc28f14209d91ebd945d
Giovani Moutinho <giovanim@ymail.com>
2019-10-25 14:12:38 -0300

    Corrige rotas e formata componentes

--- src/App.js
+++ src/App.js
@@ -3,26 +3,22 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import PokemonDetails from './components/PokemonDetails';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 
 function App() {
-    return (
-        <Router>
-            <div className='App'>
-                <NavBar />
-                <div className='container'>
-                    <Switch>
-                        <Route exact path='/' component={Home} />
-                        <Route
-                            exact
-                            path='/pokemon/:pokemonId'
-                            component={PokemonDetails}
-                        />
-                    </Switch>
-                </div>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className="container">
+          <Switch>
+            <Route path="/pokemon/:pokemonId" component={PokemonDetails} />
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 export default class NavBar extends Component {
-    render() {
-        return (
-            <div>
-                <nav
-                    className='navbar navbar-expand-md navbar-dark fixed-top'
-                    style={{
-                        backgroundColor: '#ef5350'
-                    }}
-                >
-                    <a
-                        href='/'
-                        className='navbar-brand col-sn-2 col-md-2 align-items-center'
-                    >
-                        Pokedex
-                    </a>
-                </nav>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <nav
+          className="navbar navbar-expand-md navbar-dark fixed-top"
+          style={{
+            backgroundColor: '#ef5350',
+          }}>
+          <a
+            href="/"
+            className="navbar-brand col-sn-2 col-md-2 align-items-center">
+            Pokedex
+          </a>
+        </nav>
+      </div>
+    );
+  }
 }
